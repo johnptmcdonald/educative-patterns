ef42ad21777a55551d43bdcca4da62aa32b074a2
John McDonald <johnptmcdonald@gmail.com>
2020-11-19 15:34:38 +0000

    adds path sum

--- DFS/path_sum.py
+++ DFS/path_sum.py
@@ -1,3 +1,6 @@
+# https://www.educative.io/courses/grokking-the-coding-interview/RMlGwgpoKKY
+# Given a binary tree and a number ‘S’, find if the tree has a path from root-to-leaf such that the sum of all the node values of that path equals ‘S’.
+
 class TreeNode:
   def __init__(self, val, left=None, right=None):
     self.val = val
